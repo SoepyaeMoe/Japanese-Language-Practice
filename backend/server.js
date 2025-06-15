b04ff27f75d089827a3e10c1ba6c8808fd18216d
Soe Pyae Moe <jre.soepyae.com>
2025-06-15 17:57:06 +0630

    priject setup done

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+
+const __dirname = path.resolve();
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
+}
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://127.0.0.1:${PORT}`);
+});
