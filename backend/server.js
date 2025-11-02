dfd4d6802990d5a1d0049f3aa573991c4625f2e0
Soe Pyae Moe <139026717+SoepyaeMoe@users.noreply.github.com>
2025-11-02 18:59:22 +0630

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,6 @@ const PORT = process.env.PORT || 8000;
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-    // serve index.html for all routes to support client-side routing on refresh
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
     });
