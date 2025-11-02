2d1070f4c7d66d9aad438078365370ce6365780c
Soe Pyae Moe <139026717+SoepyaeMoe@users.noreply.github.com>
2025-11-02 19:15:40 +0630

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 8000;
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
-    app.get('*', (req, res) => {
+    app.get('/*', (_req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
     });
 }
