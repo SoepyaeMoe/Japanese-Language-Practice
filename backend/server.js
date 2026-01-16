31ceba3739d33cf7855bda071d5e3b37201a6161
Soe Pyae Moe <139026717+SoepyaeMoe@users.noreply.github.com>
2026-01-16 14:37:46 +0630

    Fix indentation and formatting in server.js

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,9 @@ const PORT = process.env.PORT || 8000;
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'frontend/dist')));
     app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
-};
+        res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://127.0.0.1:${PORT}`);
