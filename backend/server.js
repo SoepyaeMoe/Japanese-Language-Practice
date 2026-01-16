d2e0e3e65eaec6e1cf06d2f40396e73adfa26106
Soe Pyae Moe <139026717+SoepyaeMoe@users.noreply.github.com>
2026-01-16 14:10:46 +0630

    Fix root route handler for production environment

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 8000;
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 
-    app.get('', (_req, res) => {
+    app.get('/', (_req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
     });
 }
